11a0a528ce2a2662ad7d562d437a078b4b2e6a99
Agababyan Artur <artur.agababyan04@gmail.com>
2025-12-29 22:20:52 +0100

    Refactor: Passage du modèle au format Pickle (.pkl)

--- app/models/ml_model.py
+++ app/models/ml_model.py
@@ -1,18 +1,20 @@
-import joblib
+import pickle
 from pathlib import Path
+import os
 
-# Définition des chemins relatifs
-# __file__ est ce fichier (ml_model.py).
-# On remonte de 3 niveaux : models -> app -> racine
+# Définition du chemin (attention à l'extension .pkl)
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-MODEL_PATH = BASE_DIR / "model" / "mon_modele.joblib"
+MODEL_PATH = BASE_DIR / "model" / "mon_modele.pkl"
 
-print(f"Chargement du modèle depuis : {MODEL_PATH}")
+print(f"Chargement du modèle Pickle depuis : {MODEL_PATH}")
 
 try:
-    # On charge le modèle dans la variable 'ml_model' qui sera exportée
-    ml_model = joblib.load(MODEL_PATH)
-    print("✅ Modèle chargé avec succès dans le module app.models.ml_model")
+    # --- CHANGEMENT ICI ---
+    # On utilise le context manager 'with open' pour lire en mode binaire ('rb')
+    with open(MODEL_PATH, "rb") as f:
+        ml_model = pickle.load(f)
+    print("✅ Modèle Pickle chargé avec succès.")
 except Exception as e:
-    print(f"❌ ERREUR : Impossible de charger le modèle : {e}")
+    print(f"❌ ERREUR : Impossible de charger le modèle Pickle : {e}")
     ml_model = None
+    
